33a58c78b9b0c26deedc54a9ba869c253db60381
sm-amoled <moktak072@naver.com>
2025-03-10 18:48:45 +0900

    FEAT: Pretendard 폰트 및 폰트 스타일 추가

---
+++ constants/Typography.ts
@@ -0,0 +1,91 @@
+import { StyleSheet } from "react-native";
+
+const typography = StyleSheet.create({
+  display1: {
+    fontSize: 128,
+    lineHeight: 100,
+    fontWeight: 400,
+    fontFamily: "Pretendard",
+  },
+  display2: {
+    fontSize: 96,
+    lineHeight: 80,
+    fontWeight: 400,
+    fontFamily: "Pretendard",
+  },
+  title1: {
+    fontSize: 48,
+    lineHeight: 48,
+    fontWeight: 500,
+    fontFamily: "Pretendard",
+  },
+  title2: {
+    fontSize: 40,
+    lineHeight: 40,
+    fontWeight: 700,
+    fontFamily: "Pretendard",
+  },
+  title3: {
+    fontSize: 32,
+    lineHeight: 32,
+    fontWeight: 800,
+    fontFamily: "Pretendard",
+  },
+  heading1: {
+    fontSize: 24,
+    lineHeight: 24,
+    fontWeight: 700,
+    fontFamily: "Pretendard",
+  },
+  heading2: {
+    fontSize: 20,
+    lineHeight: 24,
+    fontWeight: 700,
+    fontFamily: "Pretendard",
+  },
+  headline: {
+    fontSize: 18,
+    lineHeight: 21,
+    fontWeight: 700,
+    fontFamily: "Pretendard",
+  },
+  body: {
+    fontSize: 16,
+    lineHeight: 25.6,
+    fontWeight: 600,
+    fontFamily: "Pretendard",
+  },
+  body2: {
+    fontSize: 14,
+    lineHeight: 21,
+    fontWeight: 600,
+    fontFamily: "Pretendard",
+  },
+  label1: {
+    fontSize: 14,
+    lineHeight: 18,
+    fontWeight: 700,
+    fontFamily: "Pretendard",
+  },
+  label2: {
+    fontSize: 13,
+    lineHeight: 18,
+    fontWeight: 600,
+    letterSpacing: -0.08,
+    fontFamily: "Pretendard",
+  },
+  label3: {
+    fontSize: 12,
+    lineHeight: 18,
+    fontWeight: 600,
+    fontFamily: "Pretendard",
+  },
+  label4: {
+    fontSize: 10,
+    lineHeight: 10,
+    fontWeight: 600,
+    fontFamily: "Pretendard",
+  },
+});
+
+export default typography;
