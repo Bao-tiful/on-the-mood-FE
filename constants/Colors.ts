31a25e803b9d8543557f732e9a6f3d0968fb8718
sm-amoled <moktak072@naver.com>
2025-03-10 19:04:41 +0900

    FEAT: 색상코드 추가

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,14 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+  black100: "#1E1E1E",
+  black40: "#1E1E1E66",
+  black32: "#1E1E1E51",
+  black18: "#1E1E1E2E",
+  gray: "#8E8E93",
+  lightGray: "#F5F5F5",
+  white40: "#FFFFFF66",
+  white100: "#FFFFFF",
+  modalBackground: "#383838BF",
 };
