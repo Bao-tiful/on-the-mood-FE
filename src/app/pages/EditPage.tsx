db7893b8cd2e4ec5579b853d9acca597c501ff75
sm-amoled <moktak072@naver.com>
2025-03-12 22:01:28 +0900

    FEAT: 노트 작성 컴포넌트 분리

--- src/app/pages/EditPage.tsx
+++ src/app/pages/EditPage.tsx
@@ -8,7 +8,7 @@ import {
   View,
 } from "react-native";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TemperatureSlider from "@/src/components/editpage/TemperatureSlider";
 import { ToolbarButton } from "@/src/components/ToolbarButton";
 import { IconName } from "@/src/components/Icon";
@@ -17,6 +17,7 @@ import { Colors } from "@/constants/Colors";
 import typography from "@/constants/Typography";
 import LocationAndTemperature from "@/src/components/editpage/LocationAndTemperature";
 import Keywords from "@/src/components/editpage/Keywords";
+import NoteEditor from "@/src/components/editpage/NoteEditor";
 
 const EditPage = () => {
   const { date, temperature } = useLocalSearchParams();
@@ -27,6 +28,8 @@ const EditPage = () => {
   const parsedTemperature = Array.isArray(temperature)
     ? Number(temperature[0])
     : Number(temperature);
+
+  const [memo, setMemo] = useState("");
 
   return (
     // TODO: 여기에서 색상 변경해주기
@@ -56,34 +59,17 @@ const EditPage = () => {
           </View>
 
           <View style={{ marginTop: 16 }}>
-            <View style={{ alignItems: "center" }}>
-              <TemperatureSlider
-                feelsLikeTemp={parsedTemperature}
-                changeMoodTemp={() => {}}
-              />
-            </View>
+            <TemperatureSlider
+              feelsLikeTemp={parsedTemperature}
+              changeMoodTemp={() => {}}
+            />
           </View>
           <View style={{ flex: 1 }}>
-            <View style={styles.noteEditorContainer}>
-              <Keywords keywordList={["키워드 1", "키워드 2", "키워드 3"]} />
-              <TextInput
-                style={styles.noteEditor}
-                multiline={true}
-                numberOfLines={3}
-                maxLength={100}
-                placeholder={
-                  "오늘 나만의 온도는 어땠나요?\n오늘의 하루를 컬러와 간단한 문장으로 표현해보세요."
-                }
-                placeholderTextColor={Colors.black40}
-                autoFocus
-              />
-              <View
-                style={{ flexDirection: "row", justifyContent: "flex-end" }}
-              >
-                <Text style={styles.noteCountingLabel}>0</Text>
-                <Text style={styles.noteMaxLabel}> /100</Text>
-              </View>
-            </View>
+            <NoteEditor
+              keywordList={["키워드 1", "keyword", "hello"]}
+              memo={memo}
+              onMemoChanged={(memo) => setMemo(memo)}
+            />
           </View>
         </KeyboardAvoidingView>
       </SafeAreaView>
@@ -105,19 +91,4 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
   },
-
-  noteEditorContainer: {
-    paddingVertical: 24,
-    paddingHorizontal: 16,
-    gap: 16,
-    backgroundColor: Colors.white40,
-    borderRadius: 16,
-  },
-  noteEditor: {
-    ...typography.body,
-    textAlignVertical: "top",
-    minHeight: 64,
-  },
-  noteCountingLabel: { ...typography.label1, color: Colors.black100 },
-  noteMaxLabel: { ...typography.label1, color: Colors.black40 },
 });
