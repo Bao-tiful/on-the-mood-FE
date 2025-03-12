e5a5b6d5eb9b72e3ebe7d4d37444e11f19a1fcce
sm-amoled <moktak072@naver.com>
2025-03-13 00:35:23 +0900

    FEAT: 노트 저장 및 목록 불러오는 api 로직 추가

--- src/api/endpoints/noteApi.ts
+++ src/api/endpoints/noteApi.ts
@@ -1,1 +1,38 @@
-//
+import axios from "axios";
+import axiosClient from "../clients/axiosClient";
+import { handleApiError } from "../apiUtils";
+
+interface PostNotesProps {
+  location: string;
+  content: string;
+  custom_temp: number;
+}
+
+export const postNote = async (prop: PostNotesProps) => {
+  try {
+    const response = await axiosClient.post<Note>("/daily-notes", prop);
+    return response.data;
+  } catch (error) {
+    handleApiError(error);
+    throw error;
+  }
+};
+
+interface Note {
+  id: string;
+  location: string;
+  content: string;
+  custom_temp: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export const getNotes = async () => {
+  try {
+    const response = await axiosClient.get<Note[]>("/daily-notes");
+    return response.data;
+  } catch (error) {
+    handleApiError(error);
+    throw error;
+  }
+};
