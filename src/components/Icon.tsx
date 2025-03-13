9cca20f900c2d1dc3b85e789beb224c8c2648a5f
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2025-03-13 09:09:15 +0900

    [구조 개편] 디렉토리 구조 및 state 선언 위치 변경 (#5)
    * dependency: 패키지 업데이트
    
    * refactor: 폴더 구조 변경
    
    * refactor: remove unused import
    
    * refactor: add guide note
    
    * refactor: date 상태값 루트 페이지로 끌어올리기
    
    * feat: add PR 템플릿
    
    * release: v0.0.0.1

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -17,7 +17,7 @@ import TemperatureGray from "@assets/icons/temperature_gray.svg";
 import Trash from "@assets/icons/trash.svg";
 import DownWhite from "@assets/icons/down_white.svg";
 import TemperatureGray from "@assets/icons/temperature_gray.svg";
-import { Colors } from "@/constants/Colors";
+import { Colors } from "@/src/styles/Colors";
 import { ColorValue } from "react-native";
 import Svg, { Mask, Rect } from "react-native-svg";
 
