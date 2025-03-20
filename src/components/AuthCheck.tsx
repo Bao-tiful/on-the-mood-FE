78e8e3c0910b4b0e94739df9c79630fea1f903ab
sm-amoled <moktak072@naver.com>
2025-03-20 17:46:27 +0900

    feat: AuthCheck 컴포넌트에서 redirectPath 분리

--- src/components/AuthCheck.tsx
+++ src/components/AuthCheck.tsx
@@ -1,19 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { router, useNavigation } from "expo-router";
+import { Href, router, useNavigation } from "expo-router";
 
 /// Auth 상태를 체크하고, 토큰이 저장되어있지 않으면 로그인 페이지로 변경해준다.
-const AuthCheck = ({ children }: { children: React.ReactNode }) => {
+const AuthCheck = ({
+  children,
+  redirectPath,
+}: {
+  children: React.ReactNode;
+  redirectPath: Href;
+}) => {
   const navigation = useNavigation();
 
   useEffect(() => {
     const checkAuthState = async () => {
       try {
         const token = await AsyncStorage.getItem("accessToken");
-        if (!token)
-          //
-          router.replace("/pages/LoginPage");
+        // if (!token) {
+        router.replace(redirectPath);
+        // }
       } catch (error) {
         console.error("토큰 확인 중 오류 발생 :", error);
       }
