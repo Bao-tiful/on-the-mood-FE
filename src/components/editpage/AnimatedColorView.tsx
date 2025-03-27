681ab17c401e513940a26bc7f48f7f0c7a4d51cc
sm-amoled <moktak072@naver.com>
2025-03-27 18:11:39 +0900

    refactor: AnimatedColorView 컴포넌트를 함수형으로 전환

--- src/components/editpage/AnimatedColorView.tsx
+++ src/components/editpage/AnimatedColorView.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useRef } from "react";
 import {
   Animated,
   Easing,
@@ -9,93 +9,68 @@ import {
 } from "react-native";
 
 interface AnimatedColorViewProps {
-  duration: number;
-  colors: string[];
-  activeIndex: number;
-
-  style: ViewStyle;
   children: React.ReactNode;
-  easing: EasingFunction;
+  activeIndex: number;
+  duration?: number;
+  colors?: string[];
+  style?: ViewStyle;
+  easing?: EasingFunction;
 }
 
-export default class AnimatedColorView extends Component<AnimatedColorViewProps> {
-  static defaultProps: Partial<AnimatedColorViewProps> = {
-    duration: 200,
-    colors: [],
-    activeIndex: 0,
-    style: {},
-    easing: Easing.linear,
-  };
-
-  // 애니메이션을 적용할 색상 팔레트
+const AnimatedColorView = ({
+  children,
+  activeIndex,
+  duration = 400,
+  colors = [],
+  style = {},
+  easing = Easing.ease,
+}: AnimatedColorViewProps) => {
+  // 애니메이션 값을 저장할 색상팔레트 레퍼런스 배열 생성
   // Value는 투명도(opacity) -> 1이면 색상이 보이고 0이면 색상이 보이지 않음
-  animatedValues: Animated.Value[] = [];
-  setInterval: any = null;
+  const animatedValues = useRef(
+    colors.map(() => new Animated.Value(0))
+  ).current;
 
-  // prop으로 들어온 색상 hex 코드 목록을 animatedValues에 적용
-  constructor(props: any) {
-    super(props);
-    const { colors, activeIndex } = props;
-    if (colors.length) {
-      colors.map((_item: string) => {
-        this.animatedValues.push(new Animated.Value(0));
-      });
-      this.animatedValues[activeIndex].setValue(1);
-    }
-  }
-
-  // 특정 index의 색상을 업데이트 시킬 때
-  // 에니메이션을 적용하며 해당 색상의 투명도를 1으로, 나머지 색상은 0으로 set
-  setActive = async (index: number) => {
-    const { duration, easing } = this.props;
-    this.animatedValues.map((_, i) => {
-      if (index !== i) {
-        Animated.timing(this.animatedValues[i], {
-          toValue: 0,
-          duration,
-          useNativeDriver: true,
-          easing: easing,
-        }).start();
-      } else {
-        Animated.timing(this.animatedValues[index], {
-          toValue: 1,
-          duration,
-          useNativeDriver: true,
-          easing: easing,
-        }).start();
-      }
+  // 활성화된 색상을 업데이트하는 함수
+  const setActive = (index: number) => {
+    animatedValues.forEach((animatedValue, i) => {
+      Animated.timing(animatedValue, {
+        toValue: i === index ? 1 : 0,
+        duration,
+        useNativeDriver: true,
+        easing,
+      }).start();
     });
   };
 
-  componentDidUpdate(props: any) {
-    const { activeIndex } = this.props;
-
-    if (props.activeIndex !== activeIndex) {
-      this.setActive(activeIndex);
+  // 컴포넌트 마운트 시 초기 활성 색상 설정
+  useEffect(() => {
+    if (colors.length > 0) {
+      setActive(activeIndex);
     }
-  }
+  }, [colors, activeIndex]);
 
-  render() {
-    const { colors, children } = this.props;
-    const props = this.props;
+  // activeIndex가 변경될 때마다 애니메이션 업데이트
+  useEffect(() => {
+    setActive(activeIndex);
+  }, [activeIndex]);
 
-    return (
-      <View {...props}>
-        {colors.map((item, index) => {
-          // 애니메이션이 적용된 opacity 값으로 색상을 렌더링
-          const opacity = this.animatedValues[index];
-          return (
-            <Animated.View
-              key={`animated-view-${index}`}
-              style={[
-                StyleSheet.absoluteFill,
-                { backgroundColor: item, opacity },
-              ]}
-            />
-          );
-        })}
-        {children}
-      </View>
-    );
-  }
-}
+  return (
+    <View style={style}>
+      {colors.map((item, index) => (
+        // 애니메이션이 적용된 opacity 값으로 색상을 렌더링
+
+        <Animated.View
+          key={`animated-view-${index}`}
+          style={[
+            StyleSheet.absoluteFill,
+            { backgroundColor: item, opacity: animatedValues[index] },
+          ]}
+        />
+      ))}
+      {children}
+    </View>
+  );
+};
+
+export default AnimatedColorView;
