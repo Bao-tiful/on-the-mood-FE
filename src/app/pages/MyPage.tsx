5daaf73aad207538d3f6ddd2c74923329ba0d4b5
sm-amoled <moktak072@naver.com>
2025-04-01 23:34:07 +0900

    feat: MyPage 버튼 배치

--- src/app/pages/MyPage.tsx
+++ src/app/pages/MyPage.tsx
@@ -1,9 +1,17 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  SectionList,
+  StyleSheet,
+  Switch,
+  Text,
+  View,
+} from "react-native";
 import React, { useEffect, useState } from "react";
-import { OndoColors } from "@/src/styles/Colors";
+import { Colors, OndoColors } from "@/src/styles/Colors";
 import { router, useLocalSearchParams } from "expo-router";
 import { ToolbarButton } from "@/src/components/ToolbarButton";
-import { IconName } from "@/src/components/Icon";
+import Icon, { IconName } from "@/src/components/Icon";
 import typography from "@/src/styles/Typography";
 import { toDateString } from "@/src/utils/dateUtils";
 
@@ -35,7 +43,6 @@ const MyPage = () => {
     >
       <SafeAreaView style={{ gap: 20, margin: 12 }}>
         <View style={{ gap: 4 }}>
-          {/* <Text>MyPage</Text> */}
           <View style={styles.topToolbar}>
             <ToolbarButton
               name={IconName.back}
@@ -53,6 +60,81 @@ const MyPage = () => {
             >
               내 정보
             </Text>
+          </View>
+        </View>
+        <View style={{ gap: 16 }}>
+          <View style={{ gap: 24 }}>
+            {/* SignIn Info */}
+            <View
+              style={{
+                paddingVertical: 12,
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 16,
+              }}
+            >
+              <View
+                style={{
+                  width: 24,
+                  height: 24,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: Colors.white100,
+                  borderRadius: "50%",
+                }}
+              >
+                <Icon name={IconName.googleLogo} size={12} />
+              </View>
+              <Text style={typography.headline}>hongildong@naver.com</Text>
+            </View>
+            <View
+              style={{
+                height: 1,
+                backgroundColor: Colors.black18,
+              }}
+            />
+          </View>
+          <View style={{ gap: 24 }}>
+            <View style={{ marginVertical: 16 }}>
+              <Text style={typography.headline}>알림 설정</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={typography.body}>기록 시간 알림</Text>
+              <Switch />
+            </View>
+            <View
+              style={{
+                height: 1,
+                backgroundColor: Colors.black18,
+              }}
+            />
+          </View>
+          <View style={{ gap: 24 }}>
+            <View style={{ marginVertical: 16 }}>
+              <Text style={typography.headline}>화면 잠금</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={typography.body}>비밀번호</Text>
+              <Switch />
+            </View>
+            <View
+              style={{
+                height: 1,
+                backgroundColor: Colors.black18,
+              }}
+            />
           </View>
         </View>
       </SafeAreaView>
