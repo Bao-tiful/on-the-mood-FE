f98bc6b52dcbea6f636495b5085f68547b1f3d01
sm-amoled <moktak072@naver.com>
2025-05-14 18:15:01 +0900

    feat: 기본 UI 구조 작성

--- src/app/pages/Auth/Withdraw.tsx
+++ src/app/pages/Auth/Withdraw.tsx
@@ -6,10 +6,11 @@ import Icon, { IconName } from "@/src/components/Icon";
 import { router } from "expo-router";
 import typography from "@/src/styles/Typography";
 import SignInButton from "@/src/components/login/SignInButton";
+import { Colors } from "@/src/styles/Colors";
 
 const Withdraw = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.topToolbar}>
         <ToolbarButton
           name={IconName.back}
@@ -20,24 +21,56 @@ const Withdraw = () => {
         <Text style={typography.heading2}>회원 탈퇴</Text>
         <View style={{ width: 44 }} />
       </View>
-      <View>
-        <Icon name={IconName.calendar} size={100}></Icon>
-        <Text>탈퇴 전 확인하세요.</Text>
-        <View>
-          <Text>
-            가입시 수집한 개인정보(이메일)를 포함하여 {"\n"}
-            작성한 모든 일기가 영구적으로 삭제되며 {"\n"}
-            다시는 복구할 수 없습니다.
-          </Text>
+      <View style={styles.content}>
+        <View style={{ alignItems: "center" }}>
+          <Icon name={IconName.caution} size={100}></Icon>
+          <View style={{ height: 32 }} />
+          <Text style={typography.heading2}>탈퇴 전 확인하세요.</Text>
+          <View style={{ height: 16 }} />
+          <View
+            style={{
+              backgroundColor: Colors.black18,
+              width: "100%",
+              padding: 24,
+              borderRadius: 16,
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={[
+                typography.body2,
+                { color: Colors.black40, textAlign: "center" },
+              ]}
+            >
+              가입시 수집한 개인정보(이메일)를 포함하여 {"\n"}
+              작성한 모든 일기가 영구적으로 삭제되며 {"\n"}
+              다시는 복구할 수 없습니다.
+            </Text>
+          </View>
         </View>
-      </View>
-      <View>
-        <TouchableOpacity>
-          <Icon name={IconName.check} size={24} />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottomSheetButton} onPress={() => {}}>
-          <Text>탈퇴하기</Text>
-        </TouchableOpacity>
+        <View style={{ alignItems: "center", gap: 24 }}>
+          <TouchableOpacity>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
+            >
+              <Icon name={IconName.uncheckCircle} size={24} />
+              <Text style={[typography.body2]}>
+                안내사항을 확인하였으며 이에 동의합니다.
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.bottomSheetButton,
+              { backgroundColor: Colors.black100 },
+            ]}
+            onPress={() => {}}
+          >
+            <Text style={[typography.body, { color: Colors.white100 }]}>
+              탈퇴하기
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -54,7 +87,12 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
   },
-
+  content: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: 24,
+    paddingTop: 40,
+  },
   bottomSheetButton: {
     backgroundColor: "orange",
 
