8580dbef8332fef24a1782c68193d145cf7d3c84
sm-amoled <moktak072@naver.com>
2025-05-14 18:56:19 +0900

    feat: Withdraw 페이지 스타일 코드 정리

--- src/app/pages/Auth/Withdraw.tsx
+++ src/app/pages/Auth/Withdraw.tsx
@@ -1,14 +1,15 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ToolbarButton } from "@/src/components/ToolbarButton";
 import Icon, { IconName } from "@/src/components/Icon";
 import { router } from "expo-router";
 import typography from "@/src/styles/Typography";
-import SignInButton from "@/src/components/login/SignInButton";
 import { Colors } from "@/src/styles/Colors";
 
 const Withdraw = () => {
+  const [readCaution, setReadCaution] = useState(false);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.topToolbar}>
@@ -22,49 +23,50 @@ const Withdraw = () => {
         <View style={{ width: 44 }} />
       </View>
       <View style={styles.content}>
+        {/* 안내문구 */}
         <View style={{ alignItems: "center" }}>
           <Icon name={IconName.caution} size={100}></Icon>
           <View style={{ height: 32 }} />
           <Text style={typography.heading2}>탈퇴 전 확인하세요.</Text>
           <View style={{ height: 16 }} />
-          <View
-            style={{
-              backgroundColor: Colors.black18,
-              width: "100%",
-              padding: 24,
-              borderRadius: 16,
-              alignItems: "center",
-            }}
-          >
-            <Text
-              style={[
-                typography.body2,
-                { color: Colors.black40, textAlign: "center" },
-              ]}
-            >
+          <View style={styles.cautionBox}>
+            <Text style={styles.caution}>
               가입시 수집한 개인정보(이메일)를 포함하여 {"\n"}
               작성한 모든 일기가 영구적으로 삭제되며 {"\n"}
               다시는 복구할 수 없습니다.
             </Text>
           </View>
         </View>
+        {/* 하단버튼 */}
         <View style={{ alignItems: "center", gap: 24 }}>
-          <TouchableOpacity>
-            <View
-              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
-            >
-              <Icon name={IconName.uncheckCircle} size={24} />
+          <TouchableOpacity
+            onPress={() => {
+              setReadCaution(!readCaution);
+            }}
+          >
+            <View style={styles.cautionAgreeToggle}>
+              <Icon
+                name={
+                  readCaution ? IconName.checkCircle : IconName.uncheckCircle
+                }
+                size={24}
+              />
               <Text style={[typography.body2]}>
                 안내사항을 확인하였으며 이에 동의합니다.
               </Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
+            disabled={!readCaution}
             style={[
               styles.bottomSheetButton,
-              { backgroundColor: Colors.black100 },
+              {
+                backgroundColor: readCaution ? Colors.black100 : Colors.black18,
+              },
             ]}
-            onPress={() => {}}
+            onPress={() => {
+              // 회원 탈퇴 로직 구현하기
+            }}
           >
             <Text style={[typography.body, { color: Colors.white100 }]}>
               탈퇴하기
@@ -95,7 +97,6 @@ const styles = StyleSheet.create({
   },
   bottomSheetButton: {
     backgroundColor: "orange",
-
     width: "100%",
     alignContent: "center",
     justifyContent: "center",
@@ -103,4 +104,13 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 50,
   },
+  cautionBox: {
+    backgroundColor: Colors.black18,
+    width: "100%",
+    padding: 24,
+    borderRadius: 16,
+    alignItems: "center",
+  },
+  caution: { ...typography.body2, textAlign: "center", color: Colors.black40 },
+  cautionAgreeToggle: { flexDirection: "row", alignItems: "center", gap: 8 },
 });
