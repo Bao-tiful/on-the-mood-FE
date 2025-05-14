417e7822f0aeb0df464de7022e0182d94a3e16fe
sm-amoled <moktak072@naver.com>
2025-05-14 18:56:19 +0900

    feat: Withdraw 페이지 스타일 코드 정리

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -53,7 +53,7 @@ export default function HomeScreen() {
             name={IconName.profile}
             onPress={async () => {
               // 디버깅을 위해 해당 버튼을 누르면 로그인 페이지로 이동하도록 연결
-              router.push("/pages/Auth/Withdraw");
+              router.replace("/pages/Auth/Entrance");
             }}
           />
           <ToolbarButton
