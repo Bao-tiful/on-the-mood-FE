7903d337ac9b2a12ede5838995ca1ed1c1fa1f4a
sm-amoled <moktak072@naver.com>
2025-06-29 01:45:45 +0900

    feat: Entrance 페이지 레이아웃 수정

--- src/app/pages/Auth/Entrance.tsx
+++ src/app/pages/Auth/Entrance.tsx
@@ -10,69 +10,72 @@ import { Colors } from "@/src/styles/Colors";
 import typography from "@/src/styles/Typography";
 import SignInButton, { SignInType } from "@/src/components/login/SignInButton";
 import { router } from "expo-router";
+import Logo from "@/src/components/Logo";
 
 const EntrancePage = () => {
   return (
     <View style={{ flex: 1, backgroundColor: Colors.lightGray }}>
-      <SafeAreaView style={styles.container}>
-        <View style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
           <View style={styles.logoContainer}>
-            <Text style={[typography.title2, { color: Colors.black100 }]}>
-              OntheMood
-            </Text>
-            <Text style={[typography.headline, { color: Colors.black100 }]}>
-              온도로 느끼고, 색으로 기록하다.
+            <Logo width={186} />
+            <Text style={styles.catchphrase}>
+              <Text style={styles.catchphraseEmphasis}>온도</Text>로 느끼고,
+              {"\n"}
+              <Text style={styles.catchphraseEmphasis}>색</Text>
+              으로 기록하다.
             </Text>
           </View>
-        </View>
-        <View style={styles.signInButtonContainer}>
-          <SignInButton
-            type={SignInType.google}
-            onPress={() => {
-              console.log("google");
-            }}
-          />
-          <SignInButton
-            type={SignInType.apple}
-            onPress={() => {
-              console.log("apple");
-            }}
-          />
-          {/* TODO: 이메일로 로그인 부분은 나중에 디자인 나오면 수정하기 */}
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-              gap: 16,
-              paddingHorizontal: 40,
-            }}
-          >
-            <TouchableOpacity
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
+
+          <View style={styles.signInButtonContainer}>
+            <SignInButton
+              type={SignInType.google}
               onPress={() => {
-                router.push("/pages/Auth/SignUp");
+                console.log("google");
+              }}
+            />
+            <SignInButton
+              type={SignInType.apple}
+              onPress={() => {
+                console.log("apple");
+              }}
+            />
+            {/* TODO: 이메일로 로그인 부분은 나중에 디자인 나오면 수정하기 */}
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: 16,
+                paddingHorizontal: 40,
               }}
             >
-              <Text>회원가입</Text>
-            </TouchableOpacity>
-            <Text>|</Text>
-            <TouchableOpacity
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-              onPress={() => {
-                router.push("/pages/Auth/SignIn");
-              }}
-            >
-              <Text>이메일로 로그인</Text>
-            </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onPress={() => {
+                  router.push("/pages/Auth/SignUp");
+                }}
+              >
+                <Text>회원가입</Text>
+              </TouchableOpacity>
+              <Text>|</Text>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onPress={() => {
+                  router.push("/pages/Auth/SignIn");
+                }}
+              >
+                <Text>이메일로 로그인</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </SafeAreaView>
@@ -85,20 +88,30 @@ export default EntrancePage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
-    marginVertical: 120,
+    marginVertical: 20,
+    paddingHorizontal: 16,
+    gap: 40,
     flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-between",
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
   },
   logoContainer: {
-    top: "25%",
-    alignItems: "center",
+    alignItems: "flex-start",
     justifyContent: "center",
     gap: 16,
+    paddingHorizontal: 24,
   },
   signInButtonContainer: {
     width: "100%",
     gap: 16,
+  },
+  catchphrase: {
+    ...typography.headline,
+    color: Colors.black40,
+    lineHeight: 26,
+  },
+  catchphraseEmphasis: {
+    fontWeight: "bold",
+    color: Colors.black70,
   },
 });
