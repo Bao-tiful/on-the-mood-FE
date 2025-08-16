ec8569c08664c9e25ae343b9a0e195e9e3e902f0
sm-amoled <moktak072@naver.com>
2025-08-16 14:18:20 +0900

    feat: icon 에셋 추가 및 색상 수정

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 import Arrow from '@assets/icons/arrow.svg';
 import Back from '@assets/icons/back.svg';
 import Calendar from '@assets/icons/calendar.svg';
+import Cancel from '@assets/icons/cancel.svg';
 import Check from '@assets/icons/check.svg';
 import Down from '@assets/icons/down.svg';
 import DownWhite from '@assets/icons/down_white.svg';
 import Edit from '@assets/icons/edit.svg';
+import Eye from '@assets/icons/eye.svg';
 import Info from '@assets/icons/info.svg';
+import KeypadBack from '@assets/icons/keypad_back.svg';
 import List from '@assets/icons/list.svg';
 import Location from '@assets/icons/location.svg';
 import Plus from '@assets/icons/plus.svg';
 import Profile from '@assets/icons/profile.svg';
+import Sad from '@assets/icons/sad.svg';
 import Temperature from '@assets/icons/temperature.svg';
 import TemperatureGray from '@assets/icons/temperature_gray.svg';
 import Trash from '@assets/icons/trash.svg';
@@ -25,14 +29,18 @@ export enum IconName {
   arrow = 'arrow',
   back = 'back',
   calendar = 'calendar',
+  cancel = 'cancel',
   check = 'check',
   down = 'down',
   edit = 'edit',
+  eye = 'eye',
   info = 'info',
+  keypadBack = 'keypad_back',
   list = 'list',
   location = 'location',
   plus = 'plus',
   profile = 'profile',
+  sad = 'sad',
   temperature = 'temperature',
   trash = 'trash',
   downWhite = 'down_white',
@@ -48,14 +56,18 @@ const icons = {
   [IconName.arrow]: Arrow,
   [IconName.back]: Back,
   [IconName.calendar]: Calendar,
+  [IconName.cancel]: Cancel,
   [IconName.check]: Check,
   [IconName.down]: Down,
   [IconName.edit]: Edit,
+  [IconName.eye]: Eye,
   [IconName.info]: Info,
+  [IconName.keypadBack]: KeypadBack,
   [IconName.list]: List,
   [IconName.location]: Location,
   [IconName.plus]: Plus,
   [IconName.profile]: Profile,
+  [IconName.sad]: Sad,
   [IconName.temperature]: Temperature,
   [IconName.trash]: Trash,
   [IconName.downWhite]: DownWhite,
